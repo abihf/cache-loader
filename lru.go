e3cec3df7b8b87d0fa8d6b29f167b2e631c215db
Abi Hafshin <abi@hafs.in>
2020-04-17 00:59:56 +0700

    Use default lru cache

--- lru.go
+++ lru.go
@@ -6,8 +6,23 @@ import (
 	lru "github.com/hashicorp/golang-lru"
 )
 
+// LRUCache wraps hashicorp's lru cache object, so it's compatible with loader cache
+type LRUCache struct {
+	*lru.Cache
+}
+
+// Add item to cache
+func (c *LRUCache) Add(key, value interface{}) {
+	c.Cache.Add(key, value)
+}
+
+// Remove item from cache
+func (c *LRUCache) Remove(key interface{}) {
+	c.Cache.Remove(key)
+}
+
 // NewLRU creates Loader with lru based cache
 func NewLRU(fn LoadFunc, ttl time.Duration, size int) *Loader {
-	cache, _ := lru.NewARC(size)
-	return New(fn, ttl, cache)
+	cache, _ := lru.New(size)
+	return New(fn, ttl, &LRUCache{cache})
 }
